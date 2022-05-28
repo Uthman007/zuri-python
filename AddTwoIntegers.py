1e30a1da34333060012bbb542ac4d45a7d3128be
Adewuyi uthman alao <adewuyiuthmanalao@gmail.com>
2022-05-28 14:26:33 +0100

    zuri python projects

---
+++ AddTwoIntegers.py
@@ -0,0 +1,4 @@
+num1 = 4
+num2 = 6
+result = num1 + num2
+print(result)
---
+++ ZuriNumOfWord.py
@@ -0,0 +1,7 @@
+Astring = "Write a python program that counts and returns the number of words in a given text"
+
+WordList = Astring.split()
+
+NumberOfWords = len(WordList)
+
+print(NumberOfWords)
