67b1d8279f2938cf33a6b791c092a2f3891cc941
Adewuyi uthman alao <adewuyiuthmanalao@gmail.com>
2022-05-28 18:20:12 +0100

    anagram code

---
+++ Finding-Anagrams/main.py
@@ -0,0 +1,17 @@
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+
+def find_anagram(word, anagram):
+    # [assignment] Add your code here
+    if(sorted(word)== sorted(anagram)):
+        print("true")
+    else:
+        print("false")
+
+# code driver
+word = "listen"
+anagram = "silent"
+find_anagram(word, anagram)
